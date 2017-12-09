08df7cb22385786d43e470f6c6886815f12e96a0
Vannadin <pylon123123@naver.com>
2017-12-09 19:31:38 +0900

    0.0.01
    수행평가 종료, 본격적인 사이트 제작 시작.

--- animation.js
+++
@@ -1,8 +0,0 @@
-function myFunction() {
-    var x = document.getElementById("myTopnav");
-    if (x.className === "head_menu") {
-        x.className += " responsive";
-    } else {
-        x.className = "head_menu";
-    }
-}
